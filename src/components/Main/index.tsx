66e395e4f3d81b7e134f617b2920550cc48c4c62
Andre <als.andresilveira@gmail.com>
2020-09-15 00:09:55 -0300

    initial tests config

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Main = () => { 
+  return (
+    <Container>
+      <h1>react avançado</h1>
+    </Container>
+  );
+};
+
+export default Main;
